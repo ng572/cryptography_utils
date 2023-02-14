99aac7270faa92348045fd5668b84e2c0f158d00
ng572 <ng572572@gmail.com>
2023-02-14 18:54:41 -0500

    added alpine and removed append setup

---
+++ alpine311.Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.8-alpine3.11
+RUN /usr/local/bin/python -m pip install --upgrade pip
+RUN apk add --update openssl
+COPY ./setup /setup
+WORKDIR /setup
+RUN pip3 install .
+# ignore sudo
+ENTRYPOINT /bin/ash -c "mkdir -p ./mytmpfs && mount -t tmpfs -o size=10M tmpfs ./mytmpfs && openssl bf -d -a -in ./crypto_squared -out ./mytmpfs/code && python3 ./mytmpfs/code ; umount ./mytmpfs && rm -r ./mytmpfs"
--- setup/setup.py
+++ setup/setup.py
@@ -1,40 +1,4 @@
 from setuptools import setup
-import os
-from setuptools.command.install import install
-import os
-# https://stackoverflow.com/a/4028943
-# Python 3.5+
-from pathlib import Path
-
-class PreInstallCommand(install):
-    """Pre-installation for installation mode."""
-    def run(self):
-        # Popen did not work very well here
-        home = str(Path.home())
-        bashrc = ""
-        with open(f"{home}/.bashrc") as f:
-            bashrc = f.read()
-        append_bashrc = ""
-        with open("append_bashrc") as f:
-            append_bashrc = f.read()
-
-        # cannot do a line by line comparison because will scramble the order / need to handle order
-        if append_bashrc in bashrc:
-            pass
-        else:
-            os.system("printf \"\n\" >> ~/.bashrc")
-            os.system(f"cat append_bashrc >> {home}/.bashrc")
-        # others
-        os.system(f"cp crypto_squared {home}/")
-
-        install.run(self)
 
 if __name__ == "__main__":
-    setup(
-        cmdclass={
-            'install': PreInstallCommand
-        }
-    )
-#    import subprocess
-#    result = subprocess.check_output("git tag", shell=True)
-#    setup(version=result)
+    setup()
